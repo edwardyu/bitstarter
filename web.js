58cbaf05a0616ec0783510aab0f2fb0b4a7ea6a6
edwardyu <edwardy20@yahoo.com>
2013-07-11 03:37:01 +0000

    web.js with buffers

--- web.js
+++ web.js
@@ -1,4 +1,17 @@
 var express = require('express');
+var fs = require('fs');
+var app = express.createServer(express.logger());
+
+app.get('/', function(request, response) {
+	var index = fs.readFileSync('index.html');
+	var buffer new Buffer(index, "utf-8");
+  response.send(buffer.toString('utf-8'));
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});var express = require('express');
 
 var app = express.createServer(express.logger());
 
