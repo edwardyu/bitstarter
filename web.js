0eb1ee576359f788f0f28f7f16b7461ef3a95487
edwardyu <edwardy20@yahoo.com>
2013-07-11 03:46:58 +0000

    update web.js

--- web.js
+++ web.js
@@ -3,20 +3,8 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-	var index = fs.readFileSync('index.html');
-	var buffer new Buffer(index, "utf-8");
-  response.send(buffer.toString('utf-8'));
-});
-
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});var express = require('express');
-
-var app = express.createServer(express.logger());
-
-app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+	var index = fs.readFileSync('index.html').toString('utf-8');
+  response.send(index);
 });
 
 var port = process.env.PORT || 5000;
