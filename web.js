8ec8645a2c5491452f715146a6ab6d6dd1180e27
edwardyu <edwardy20@yahoo.com>
2013-07-19 05:22:04 +0000

    now loads css

--- web.js
+++ web.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var fs = require('fs');
 var app = express.createServer(express.logger());
-
+app.use(express.static(__dirname));
 app.get('/', function(request, response) {
 	var index = fs.readFileSync('index.html').toString('utf-8');
   response.send(index);
