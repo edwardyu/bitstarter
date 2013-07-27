d6c781b156905a458b6d52f51565b7b76378948e
Edward Yu <edwardy20@yahoo.com>
2013-07-27 15:05:01 -0500

    scrape from amazon

---
+++ discover-products.js
@@ -0,0 +1,14 @@
+var request = require('request');
+webpage = 'http://www.reddit.com/r/AmazonUnder5/hot.json?limit=100';
+
+//get urls
+request({uri : webpage, json:true, headers:{useragent: 'rrandomize product bot v. 0.0.1'}}, function(error, response, body) {
+	if(!error && response.statusCode == 200) {
+		for(var key in body.data.children) {
+			var url = body.data.children[key].data.url;
+			var asin = url.match(/http:\/\/(?:www\.|)amazon\.com\/(?:gp\/product|[^\/]+\/dp|dp)\/([^\/]+)/);
+			console.log(asin);
+		}
+		
+	}
+});
