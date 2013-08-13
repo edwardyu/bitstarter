c23cd214c3a11d0ca8201217eef1853f967bf8a4
Edward Yu <edwardy20@yahoo.com>
2013-08-12 22:58:14 -0500

    forms5

--- web.js
+++ web.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var fs = require('fs');
 var app = express.createServer(express.logger());
+
 app.use(express.bodyParser());
 app.use(express.static(__dirname));
 
@@ -11,10 +12,26 @@ app.get('/', function(request, response) {
 
 app.post('/betalist', function(request, response) {
 	var email = request.body.email;
-	response.send(email);
+	if(isValidEmail(email)) {
+		fs.openSync('emails.txt', 'a');
+		fs.appendFile('emails.txt', email, function (err) {
+			if (err) throw err;
+		});
+
+		response.send(email + ' was appended to file!');
+	}
+	else
+		response.send("Not a valid email.");
 });
 
 var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+function isValidEmail(str) {
+	if(str.contains('@') && str.contains('.'))
+		return true;
+	else
+		return false;
+}
