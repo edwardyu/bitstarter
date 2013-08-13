aeee55681fbfb2b2a7cbb81372a0a63546d79d83
Edward Yu <edwardy20@yahoo.com>
2013-08-12 22:20:29 -0500

    forms2

--- web.js
+++ web.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var fs = require('fs');
 var app = express.createServer(express.logger());
+app.use(express.bodyParser());
 app.use(express.static(__dirname));
+
 app.get('/', function(request, response) {
 	var index = fs.readFileSync('index.html').toString('utf-8');
   response.send(index);
